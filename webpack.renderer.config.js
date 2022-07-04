a9c03694c0ccb56d2476ce320989117ed0f9bb0c
tastytim <tastytimgm@gmail.com>
2022-07-04 09:56:56 +0200

    fixed fs error

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,12 +1,14 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
+
 rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
 module.exports = {
+  target:'electron-renderer',
   module: {
     rules,
   },
